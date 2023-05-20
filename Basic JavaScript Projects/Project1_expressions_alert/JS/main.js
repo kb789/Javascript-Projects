c96e70ce866745e5207053da138cf9ab9a5a5b67
kb789 <bucher.kristina@gmail.com>
2023-05-20 14:07:00 -0400

    first commit

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,9 @@
+// window.alert creates a pop-up message
+window.alert("Hello, world!");
+//The next 2 lines creates 2 variables
+var Sent1="This is the beginning of the string";
+var Sent2=" and this is the end of the string";
+// this line concantenates 2 strings and writes the result to the webpage
+document.write(Sent1+Sent2);
+//this is an expression
+var my_expression=5+5;
