de0e05de13ac96ed9664426ca36024323494fa2e
kb789 <bucher.kristina@gmail.com>
2023-05-20 14:13:45 -0400

    fixed typo

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,6 +1,6 @@
 // window.alert creates a pop-up message
 window.alert("Hello, world!");
-//The next 2 lines creates 2 variables
+//The next 2 lines create 2 variables
 var Sent1="This is the beginning of the string";
 var Sent2=" and this is the end of the string";
 // this line concantenates 2 strings and writes the result to the webpage
