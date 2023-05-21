bfec9086314a5e6d81f4cc2aeab55f031977baf6
kb789 <bucher.kristina@gmail.com>
2023-05-20 23:50:44 -0400

    first commit functions

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,11 @@
+function talk()
+ {
+//concatenate a string using += operator
+str1="hello";
+str1+=" world";
+
+//get the element with the id of test and assign the string to it's innerhtml
+var x = document.getElementById("test");
+x.innerHTML=str1;
+
+}
