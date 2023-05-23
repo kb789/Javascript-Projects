9c0a6cf10659777dc7153bef736d70fb910d7906
kb789 <bucher.kristina@gmail.com>
2023-05-23 09:34:29 -0400

    project5

---
+++ Basic JavaScript Projects/Project5_comparisons_type_coercion/JS/main.js
@@ -0,0 +1,14 @@
+//typeof operator
+document.write(typeof(7.6));
+document.write("<br>");
+
+//expression combining a string and a number.
+var val1 = "5";
+var val2 = 10;
+var a = val1 + val2;
+document.write(a);
+document.write("<br>");
+
+//boolean and comparison operators
+var b = ((5==5) && (6===6.0)) && (!(2>3) || (5<4));
+document.write(b);  
