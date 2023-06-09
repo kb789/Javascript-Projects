d8b80f125f3ea430a7ab9da3b2b8504d93dd1612
kb789 <bucher.kristina@gmail.com>
2023-06-08 22:54:20 -0400

    third calculator

--- Calculator/JS/main.js
+++ Calculator/JS/main.js
@@ -6,38 +6,70 @@
 function numberClick(val) {
    
     // repeating decimals
+    // handles the case where a calculation has been done and the next input entered is a number=resets to zero
     if (num!==0 && op==="") {
         num=0;
         document.querySelector(".calculator-screen").value=val;
+    // handles initial case where first number is a zero
     } else if (document.querySelector(".calculator-screen").value==="0") {
         document.querySelector(".calculator-screen").value=val;
+    // handles case where user accidentally inputs 2 decimal points
+    }
+    else if (val===".") {
+        let screen = document.querySelector(".calculator-screen").value;
+        let last_char = screen[screen.length-1]
+        console.log(last_char)
+      
+        if (last_char===".") {
+            screen=screen.slice(0, screen.length-1)
+            screen+=val
+            document.querySelector(".calculator-screen").value=screen;
+            console.log(screen)
+        } 
+        else {
+            document.querySelector(".calculator-screen").value+=val;
+        }
+
     }
     else {
     document.querySelector(".calculator-screen").value+=val;
     }
+
 }
 
 function operatorClick(val) {
-        // if previous char in val is operator, replace it
+         // handles the case where a calculation has been done and the next input entered is an operator==doesn't reset to zero
         if (num!==0 && op==="") {
           op=val;
         }
+        
+        // deals with case where user accidentally enters 2 consecutive operators
         let screen = document.querySelector(".calculator-screen").value;
         let last_char = screen[screen.length-1]
-        console.log(last_char)
-    
-        if (last_char==="+" || last_char ==="-" || last_char ==="*" || last_char==="/") {
-            document.querySelector(".calculator-screen").value=val;
-        } else {
-        document.querySelector(".calculator-screen").value+=val;
+        if ((last_char==="+" || last_char ==="-" || last_char ==="*" || last_char==="/") && val != "-") {
+            screen=screen.slice(0, screen.length-1)
+            console.log(screen)
+            document.querySelector(".calculator-screen").value=screen;
+            
+        } else if (val==="-" && (last_char==="+" || last_char ==="-")) {
+            screen=screen.slice(0, screen.length-1)
+            console.log(screen)
+            document.querySelector(".calculator-screen").value=screen;
         }
+
+    
+        
+    
+
+        document.querySelector(".calculator-screen").value+=val;
+    
     }
 
 function equalsClick(val) {
     
     let equation = document.querySelector(".calculator-screen").value;
    
-
+    
     if (op!=="") {
       num=0;
     }
@@ -46,17 +78,26 @@ function equalsClick(val) {
     let ind=0;
     for (let i=0; i<equation.length;i++) {
         if (equation[i]==="+" || equation[i]==="-" || equation[i]==="*" || equation[i]==="/") {
+            
+            //distinguishes negative numbers from the minus operator
             if (i===0 && equation[i] === "-" ) {
              continue;
             }
-            if (op==='+') {
+            if (equation[i] === "-" && (equation[i-1] === '*' || equation[i-1]==="/" )) {
+                continue;
+               }
+            
+            
+               if (op==='+') {
                 num+=Number.parseFloat(equation.substring(ind,i));
             } 
             if (op==='-') {
                 num-=Number.parseFloat(equation.substring(ind,i));
             } 
             if (op==='*') {
+               
                 num*=Number.parseFloat(equation.substring(ind,i));
+              
             } 
             if (op==='/') {
                 num/=Number.parseFloat(equation.substring(ind,i));
@@ -64,9 +105,11 @@ function equalsClick(val) {
            
             op=equation[i]
             ind=i+1;
+            
         }
     }
    
+  
     if (op==='+') {
         num+=Number.parseFloat(equation.substring(ind, equation.length));
         }
@@ -80,7 +123,7 @@ function equalsClick(val) {
         num/=Number.parseFloat(equation.substring(ind, equation.length));
         }
 
-        document.querySelector(".calculator-screen").value=num;
+        document.querySelector(".calculator-screen").value=Number(num.toFixed(8));
         op="";
         
 }
